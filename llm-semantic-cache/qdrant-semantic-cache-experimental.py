d31a72314ea2a71639bc7f1b87e1f67c039c0607
Kameshwara Pavan Kumar Mantha <25398886+pavanjava@users.noreply.github.com>
2024-09-07 07:28:16 +0530

    Qdrant semantic cache experimental check (#2)
    * issue debug and identification
    
    * changed the qdrant and llm cost estimate
    
    ---------
    
    Co-authored-by: pavanmantha <pavan.mantha@thevaslabs.io>

--- llm-semantic-cache/qdrant-semantic-cache-experimental.py
+++ llm-semantic-cache/qdrant-semantic-cache-experimental.py
@@ -12,22 +12,23 @@ litellm.enable_cache()
 
 
 async def test_qdrant_semantic_cache_acompletion():
-    litellm.set_verbose = True
+    # litellm.set_verbose = True
     random_number = random.randint(
         1, 100000
     )  # add a random number to ensure it's always adding /reading from cache
 
     print("Testing Qdrant Semantic Caching with acompletion")
 
+    print(f"question: write a one sentence poem about: {random_number}")
     litellm.cache = Cache(
         type="qdrant-semantic",
         _host_type="cloud",
         qdrant_api_base=os.getenv("QDRANT_API_BASE"),
         qdrant_api_key=os.getenv("QDRANT_API_KEY"),
         qdrant_collection_name="semantic-cache",
-        similarity_threshold=0.8,
+        similarity_threshold=0.95,
         qdrant_quantization_config="binary",
-        # qdrant_semantic_cache_embedding_model='ollama/snowflake-arctic-embed:33m'
+        # qdrant_semantic_cache_embedding_model='ollama/llama3.1'
     )
 
     response1 = await litellm.acompletion(
@@ -41,11 +42,9 @@ async def test_qdrant_semantic_cache_acompletion():
         ],
         # mock_response="hello",
         max_tokens=20,
-        cache={'no-cache': False}
+        # cache={'no-cache': False}
     )
     print(f"Response1: {response1}")
-
-    random_number = random.randint(1, 100000)
 
     response2 = await litellm.acompletion(
         api_base='http://localhost:11434',
@@ -57,7 +56,7 @@ async def test_qdrant_semantic_cache_acompletion():
             }
         ],
         max_tokens=20,
-        cache={'no-cache': False}
+        # cache={'no-cache': False}
     )
     print(f"Response2: {response2}")
     # assert response1.id == response2.id
